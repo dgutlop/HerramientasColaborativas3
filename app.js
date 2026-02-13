afc7fe47f1707ba4705146ec01992cb284428f85
dgutlop <dgutlop5@gmail.com>
2026-02-13 10:30:46 +0100

    feat: load and log data.json in app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('App iniciada...')
+console.log("App iniciada...");
+
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => console.log("Datos cargados:", data))
+  .catch((error) => console.error("Error cargando data.json:", error));
